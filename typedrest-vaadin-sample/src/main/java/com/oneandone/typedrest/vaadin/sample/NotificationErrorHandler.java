288ddd27e73e5de415945d8b6aff5fc6ba8ff0a4
Bastian Eicher <bastian@eicher.net>
2015-11-11 11:10:42 +0100

    NotificationErrorHandler: Also write exceptions to log

--- typedrest-vaadin-sample/src/main/java/com/oneandone/typedrest/vaadin/sample/NotificationErrorHandler.java
+++ typedrest-vaadin-sample/src/main/java/com/oneandone/typedrest/vaadin/sample/NotificationErrorHandler.java
@@ -3,6 +3,8 @@ package com.oneandone.typedrest.vaadin.sample;
 import com.vaadin.server.ErrorEvent;
 import com.vaadin.server.ErrorHandler;
 import com.vaadin.ui.Notification;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Reports errors using {@link Notification}.
@@ -12,5 +14,6 @@ public class NotificationErrorHandler implements ErrorHandler {
     @Override
     public void error(ErrorEvent event) {
         Notification.show("error", event.getThrowable().getLocalizedMessage(), Notification.Type.ERROR_MESSAGE);
+        Logger.getLogger(NotificationErrorHandler.class.getName()).log(Level.SEVERE, null, event.getThrowable());
     }
 }
