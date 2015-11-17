531ce2cba03e5a84371178291b0fccadb4f6c72b
Bastian Eicher <bastian@eicher.net>
2015-11-17 13:59:26 +0100

    DefaultEntityLister: Added filtering

--- typedrest-vaadin/src/main/java/com/oneandone/typedrest/vaadin/DefaultEntityLister.java
+++ typedrest-vaadin/src/main/java/com/oneandone/typedrest/vaadin/DefaultEntityLister.java
@@ -1,6 +1,8 @@
 package com.oneandone.typedrest.vaadin;
 
+import com.vaadin.data.util.filter.SimpleStringFilter;
 import com.vaadin.ui.*;
+import com.vaadin.ui.Grid.*;
 import java.util.Collection;
 
 /**
@@ -26,6 +28,23 @@ class DefaultEntityLister<TEntity>
         grid.setSelectionMode(Grid.SelectionMode.MULTI);
         grid.addItemClickListener(x -> onClick((TEntity) x.getItemId()));
         grid.setWidth(100, Unit.PERCENTAGE);
+
+        HeaderRow filterRow = grid.appendHeaderRow();
+        for (Object pid : grid.getContainerDataSource().getContainerPropertyIds()) {
+            HeaderCell cell = filterRow.getCell(pid);
+
+            TextField filterField = new TextField();
+            filterField.setWidth(100, Unit.PERCENTAGE);
+
+            filterField.addTextChangeListener(change -> {
+                container.removeContainerFilters(pid);
+                if (!change.getText().isEmpty()) {
+                    container.addContainerFilter(new SimpleStringFilter(pid, change.getText(), true, false));
+                }
+            });
+            cell.setComponent(filterField);
+        }
+
         setCompositionRoot(grid);
     }
 
