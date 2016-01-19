05e5a347969c55cd98c924916f0ae219fdcc17a5
Bastian Eicher <bastian@eicher.net>
2016-01-19 15:59:51 +0100

    typedrest-vaadin: Introduced AbstractEntryComponent

---
+++ typedrest-vaadin/src/main/java/com/oneandone/typedrest/vaadin/components/AbstractEntryComponent.java
@@ -0,0 +1,43 @@
+package com.oneandone.typedrest.vaadin.components;
+
+import com.google.gwt.thirdparty.guava.common.eventbus.EventBus;
+import com.oneandone.typedrest.EntryEndpoint;
+import com.vaadin.ui.Component;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import javax.naming.OperationNotSupportedException;
+
+/**
+ * Base class for building components operating on an {@link EntryEndpoint}. Use
+ * this to build a UI that provides access to an API's top-level functionality.
+ *
+ * @param <TEndpoint> The specific type of {@link EntryEndpoint} to operate on.
+ */
+public abstract class AbstractEntryComponent<TEndpoint extends EntryEndpoint>
+        extends AbstractEndpointComponent<TEndpoint> {
+
+    /**
+     * Creates a new REST entry component.
+     *
+     * @param endpoint The REST endpoint this component operates on.
+     */
+    public AbstractEntryComponent(TEndpoint endpoint) {
+        super(endpoint, new EventBus());
+    }
+
+    @Override
+    protected void onLoad()
+            throws IOException, IllegalArgumentException, IllegalAccessException, FileNotFoundException, OperationNotSupportedException {
+        endpoint.readMeta();
+
+        setCompositionRoot(buildRoot());
+    }
+
+    /**
+     * Template method called to build the UI composition root after
+     * {@link EntryEndpoint#readMeta()} has been called.
+     *
+     * @return The UI composition root.
+     */
+    protected abstract Component buildRoot();
+}
