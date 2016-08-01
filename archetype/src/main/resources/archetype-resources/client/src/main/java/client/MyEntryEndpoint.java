244946cc9cae00b5b59d552731e1e47f94ad801e
Bastian Eicher <bastian@eicher.net>
2016-08-01 17:21:45 +0200

    Tweaked archetype

--- archetype/src/main/resources/archetype-resources/client/src/main/java/client/MyEntryEndpoint.java
+++ archetype/src/main/resources/archetype-resources/client/src/main/java/client/MyEntryEndpoint.java
@@ -6,8 +6,41 @@ import ${package}.model.*;
 
 public class MyEntryEndpoint extends EntryEndpoint {
 
-    public MyEntryEndpoint(URI uri, String username, String password) {
+    /**
+     * Creates a new entry point with no authentication.
+     *
+     * @param uri The base URI of the REST interface. Missing trailing slash
+     * will be appended automatically.
+     */
+    public PlannerEntryEndpoint(URI uri) {
+        super(uri);
+    }
+
+    /**
+     * Creates a new entry point using HTTP Basic Auth.
+     *
+     * @param uri The base URI of the REST interface. Missing trailing slash
+     * will be appended automatically.
+     * @param username The username used to authenticate against the REST
+     * interface.
+     * @param password The password used to authenticate against the REST
+     * interface.
+     */
+    public PlannerEntryEndpoint(URI uri, String username, String password) {
         super(uri, username, password);
+    }
+
+    /**
+     * Creates a new entry point using a token for authentication.
+     *
+     * @param uri The base URI of the REST interface. Missing trailing slash
+     * will be appended automatically.
+     * @param token The token used to authenticate against the REST
+     * interface.
+     */
+    public PlannerEntryEndpoint(URI uri, String token) {
+        super(uri);
+        defaultHeaders.add(new BasicHeader("Authorization", "Bearer " + token));
     }
 
     public CollectionEndpoint<MyEntity> getEntities() {
