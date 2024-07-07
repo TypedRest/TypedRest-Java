6207eea1b5b30876e453ce9ecb500e2c81ca40e6
Bastian Eicher <bastian@eicher.net>
2024-07-07 18:57:17 +0200

    Tweaked build config

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,23 @@
-﻿plugins {
-    kotlin("jvm") version "2.0.0" apply false
+﻿repositories.mavenCentral()
+
+plugins {
+    kotlin("jvm") version "2.0.0"
     kotlin("plugin.serialization") version "2.0.0" apply false
     id("org.jetbrains.dokka") version "1.9.20" apply false
 }
 
 subprojects {
+    group = "net.typedrest"
+    version = System.getenv("VERSION") ?: "1.0-SNAPSHOT"
+
+    repositories.mavenCentral()
+
     fun kotlin(module: String) = "org.jetbrains.kotlin.${module}"
     apply(plugin = kotlin("jvm"))
     apply(plugin = kotlin("plugin.serialization"))
     apply(plugin = "org.jetbrains.dokka")
 
-    repositories.mavenCentral()
-
-    group = "net.typedrest"
-    version = System.getenv("VERSION") ?: "1.0-SNAPSHOT"
+    kotlin {
+        compilerOptions.allWarningsAsErrors = true
+    }
 }
