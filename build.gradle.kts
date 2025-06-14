b498a41968a2bf747764a5c0a0591f26a9128980
Bastian Eicher <bastian@eicher.net>
2025-06-14 15:07:29 +0200

    Updated Kotlin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,8 @@
 repositories.mavenCentral()
 
 plugins {
-    kotlin("jvm") version "2.0.0"
-    kotlin("plugin.serialization") version "2.0.0" apply false
+    kotlin("jvm") version "2.1.21"
+    kotlin("plugin.serialization") version "2.1.21" apply false
     id("org.jetbrains.dokka") version "1.9.20"
 }
 
